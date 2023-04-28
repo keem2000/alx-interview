00695941e167f4467458675bc626c59a59fa63d8
keem2000 <joycelinaomari@gmail.com>
2023-04-28 10:33:10 +0000

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,22 +3,22 @@
 
 
 def pascal_triangle(n):
-            """
-    returns a list of lists of integers representing the Pascal’s triangle of n
     """
-                triangle = []
+    returns a list of lists of integers representing the s triangle of n
+    """
+    triangle = []
 
-                    # return (trianlgle if n <= 0)
-                        if n <= 0:
-                                        return triangle
-                                    for i in range(n):
-                                                    temp_list = []
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
 
-                                                            for j in range(i+1):
-                                                                                if j == 0 or j == i:
-                                                                                                        temp_list.append(1)
-                                                                                else:
-                                                                                                        temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-                                                                                                                triangle.append(temp_list)
-                                                                                                                    # print(triangle)
-                                                                                                                        return 
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
